5192ea5afe79d00839e4606c3571a5b48644f457
Tomas S. Grigera <tgrigera@iflysib.unlp.edu.ar>
2021-11-20 17:42:33 -0300

    Better Julia conventions and some doc in twolvl.jl

--- twolvl.jl
+++ twolvl.jl
@@ -1,9 +1,14 @@
-#
-# Metropolis MC of a two-level system
-#
+"""
+   Metropolis MC of a two-level system
 
-using Random;
-rng = MersenneTwister(211);
+Call `do_runs()` to simulate and plot for several temperatures,
+or see `twolevel_single!` and `twolevel_run!`
+"""
+module TwoLevel
+
+import Random
+
+rng = Random.MersenneTwister(211);
 
 #
 # Define the energy levels
@@ -12,10 +17,14 @@ E=zeros(2)
 E[1]=0.;
 E[2]=1.;
 
-#
-# MC simulation of a single system
-#
-function TwoLevelSingle!(Erun,T)
+"""
+    twolevel_single!(Erun,T)
+
+MC simulation of a two-level system at temperature `T`.  Initial state
+is the excited state, and energy for each step is written to the array
+`Erun`.  The initial size of `Erun` givis the munber of steps.
+"""
+function twolevel_single!(Erun::Array,T::Float64)
     state=2
     Erun[1]=E[state]
     p = exp(-(E[2]-E[1])/T)
@@ -26,61 +35,71 @@ function TwoLevelSingle!(Erun,T)
     end
 end
 
-#
-# Simulate Nsamp independent two-level systems at temperature T
-# Returns average energy en Eav.  Initialize Eav to an array
-# of the desired length of the runs (will be overwritten)
-function TwoLevelRun!(Eav,Nsamp,T)
+"""
+     twolevel_run!(Eav,Nsamp,T)
+
+ Do `Nsamp` independent simulations of two-level systems at
+temperature `T` (calling `twolevel_single!`).  Returns average energy
+in `Eav`, which must be initialized to a Float64 array of the desired
+length of the runs.
+ """
+function twolevel_run!(Eav,Nsamp,T)
     E=zeros(Float64,2);
     len=length(Eav);
     Erun=zeros(Float64,len);
     for n in 1:Nsamp
-        TwoLevelSingle!(Erun,T)
-        Eav.+=Erun
+        twolevel_single!(Erun,T);
+        Eav.+=Erun;
     end
-    Eav./=Nsamp
+    Eav./=Nsamp;
 end
 
-#
-# Equilibrium energy at temperature T
-#
+
+" Equilibrium energy at temperature T"
 function EqE(T)
     return (E[1]*exp(-E[1]/T)+E[2]*exp(-E[2]/T)) / (exp(-E[1]/T)+exp(-E[2]/T))
 end
 
-#
-# Define set of temperatures to simulate, legnth of each run
-# and number of samples
-#
-Temp=[10.,5.,1.,.5];
-len=100;
-Nsamp=10000;
 
-#
-# Run simulations and plot
 using CairoMakie
-GLMakie.activate!(type="svg")
 
-fig=Figure()
-ax=Axis(fig[1,1])
-for T in Temp
-    Eav=zeros(Float64,len);
-    TwoLevelRun!(Eav,Nsamp,T);
-    t=range(0,length(Eav)-1,step=1);
-    lines!(ax,t,Eav,label="T = $T");
-    lines!(ax,[t[1],t[end]],[EqE(T),EqE(T)]);
+function do_runs()
+
+    #
+    # Define set of temperatures to simulate, legnth of each run
+    # and number of samples
+    #
+    Temp=[10.,5.,1.,.5];
+    len=100;
+    Nsamp=10000;
+
+    #
+    # Run simulations and plot
+    CairoMakie.activate!(type="svg")
+
+    fig=Figure()
+    ax=Axis(fig[1,1])
+    for T in Temp
+        Eav=zeros(Float64,len);
+        twolevel_run!(Eav,Nsamp,T);
+        t=range(0,length(Eav)-1,step=1);
+        lines!(ax,t,Eav,label="T = $T");
+        lines!(ax,[t[1],t[end]],[EqE(T),EqE(T)]);
+    end
+    ax.title="Energy vs time, $Nsamp samples";
+    ax.xlabel="t";
+    ax.ylabel="E";
+
+    # Plot the exponential envelope
+    for T in Temp
+        t=range(0,len-1,step=1);
+        E∞=EqE(T);
+        tau=T/( (E[2]-E[1]) );
+        lines!(ax,t,E∞ .+ (E[2]-E∞).*exp.(-t/tau),linestyle=:dot);
+    end
+
+    axislegend();
+    fig
 end
-ax.title="Energy vs time, $Nsamp samples";
-ax.xlabel="t";
-ax.ylabel="E";
 
-# Plot the exponential envelope
-for T in Temp
-    t=range(0,len-1,step=1);
-    E∞=EqE(T);
-    tau=T/( (E[2]-E[1]) );
-    lines!(ax,t,E∞ .+ (E[2]-E∞).*exp.(-t/tau),linestyle=:dot);
-end
-
-axislegend();
-fig
+end # module
